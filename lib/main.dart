85639937723916d02e95ce5f95cf2aa6279f0c1b
hunderboy <masury2011@gmail.com>
2022-12-04 04:27:57 +0900

    패키지 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+
+import 'page/lobby.dart';
+import 'page/splash/splash.dart';
 
 
 void main() {
@@ -12,13 +14,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    /// 상태파 컬러 앱 전체 적용
-    SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(
-          /// ios
-          statusBarBrightness: Brightness.dark, // ios 상태바 텍스트 컬러 = white
-        )
-    );
+    // /// 상태파 컬러 앱 전체 적용
+    // SystemChrome.setSystemUIOverlayStyle(
+    //     const SystemUiOverlayStyle(
+    //       /// ios
+    //       statusBarBrightness: Brightness.dark, // ios 상태바 텍스트 컬러 = white
+    //     )
+    // );
 
     return GetMaterialApp(
         title: 'FunctionModule',
@@ -26,34 +28,25 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           // fontFamily: 'Pretendard',   /// 전체 Text 폰트 적용
         ),
-        // initialRoute: "/",
+        initialRoute: "/SplashLottie",
 
         getPages: [
+
+          /// 앱 첫 시작 로딩페이지
+          GetPage(
+              name: "/SplashLottie",
+              page: () => Splash(),
+              curve: const Cubic(0.4, 0.0, 0.4, 0.0),
+              transition: Transition.rightToLeft),
 
           GetPage(
               name: "/",
               page: () => const Lobby(),
               transition: Transition.rightToLeft),
-          GetPage(
-              name: "/Retrofit_Dio_Json",
-              page: () => RetrofitDioJson(),
-              transition: Transition.rightToLeft),
-          GetPage(
-              name: "/CustomCircleDialIndicator",
-              page: () => const CustomCircleDialIndicator(),
-              transition: Transition.rightToLeft),
-          GetPage(
-              name: "/PermissionHandler",
-              page: () => const PermissionHandlerPage(),
-              transition: Transition.rightToLeft),
-          GetPage(
-              name: "/Ready",
-              page: () => const Ready(),
-              transition: Transition.rightToLeft),
-          GetPage(
-              name: "/ChangeStatusBar",
-              page: () => const ChangeStatusBar(),
-              transition: Transition.rightToLeft),
+          // GetPage(
+          //     name: "/ShowPicture",
+          //     page: () => ShowPicture(),
+          //     transition: Transition.rightToLeft),
 
         ]
     );
---
+++ lib/page/splash/splash.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+import '../lobby.dart';
+
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+
+    // 화면이 시작되고 몇초간의 딜레이 후, 다음 화면으로 넘어가는 부분
+    new Future.delayed(
+    const Duration(seconds: 5),
+    () => Navigator.pushAndRemoveUntil(
+    context,
+    MaterialPageRoute(builder: (BuildContext context) => Lobby()),
+    (route) => false));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              // height: 300,
+              // width: 300,
+              child:
+              Lottie.asset(
+                  "assets/lottie/8546-aperture-logo-loading.json",
+                  controller: _controller, onLoaded: (composition) {
+                    _controller.addStatusListener((status) {
+
+                      // 애니메이션이 사라지면 다시 생성
+                      if (status == AnimationStatus.dismissed)
+                        _controller.forward();
+                      // 애니메이션이 끝나면 반대로 재생
+                      else if (status == AnimationStatus.completed)
+                        _controller.reverse();
+                    });
+
+                    // Configure the AnimationController with the duration of the
+                    // Lottie file and start the animation.
+                    _controller
+                      ..duration = composition.duration
+                      ..forward();
+                  }
+              )
+            )
+          ],
+        ),
+
+      )
+    );
+
+  }
+}
