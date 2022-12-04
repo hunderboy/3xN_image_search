d9ec883c33c15c62a067e17747c2bb470158df35
hunderboy <masury2011@gmail.com>
2022-12-05 04:57:23 +0900

    앱 세로모드 고정

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,13 @@ import 'page/lobby.dart';
 import 'page/splash.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(MyApp());
 }
 
