facb43ba01e7d79b0f23b43c324bf1d693726800
hunderboy <masury2011@gmail.com>
2022-12-06 02:19:15 +0900

    RestClient 코드 수정중

--- lib/api/rest_client.dart
+++ lib/api/rest_client.dart
@@ -1,14 +1,19 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 
-import 'data.dart';
+// import 'data.dart';
+import 'kakao_image.dart';
 
 part 'rest_client.g.dart';
 
-@RestApi(baseUrl: 'https://reqres.in/api')
+@RestApi(baseUrl: 'https://dapi.kakao.com/v2/search')
 abstract class RestClient {
   factory RestClient(Dio dio, {String baseUrl}) = _RestClient;
 
-  @GET('/users/{id}')
-  Future<User> getUser({@Path() required int id});
+  @GET('/image/{query}')
+  Future<KakaoImage> getImageDatas({
+    @Path() required query search_name,
+    @Path() size search_name,
+    @Path() page search_name,
+  });
 }
